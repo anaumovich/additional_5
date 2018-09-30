0501f9bae6d58148e2f0e384580657cc8bd4d04d
anaumovich <naumovich.anton@gmail.com>
2018-10-01 02:11:35 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let close = [],
+        index = 0,
+        a = 0;
+    let arr = str.split('');
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        close.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+    while (true) {
+        close.forEach((n) => {
+            index = str.indexOf(n);
+            if (index !== -1) {
+                arr.splice(index, 2);
+                str = arr.join('');
+            }
+        });
+        if (a === 50) break;
+        a++;
+    }
+    return (arr.length === 0) ? true : false;
+};
